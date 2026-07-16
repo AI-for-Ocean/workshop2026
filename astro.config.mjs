75e81682d51cff8017663b01fc4c6a106c565f08
Krishna Calindi <krishnacalindi@gmail.com>
2026-07-16 16:56:41 -0500

    auto deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ai-for-ocean.github.io',
+  base: '/workshop2026/',
+  output: 'static'
+});
